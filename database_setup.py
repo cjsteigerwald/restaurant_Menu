9e8eab30728301eec2ec9f88979bcf2c1c12945c
vagrant <vagrant@vagrant.vm>
2019-04-19 15:57:50 +0000

    Initial commit for restaurant rep

---
+++ database_setup.py
@@ -0,0 +1,45 @@
+
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+# instance of declarative base class
+Base = declarative_base()
+
+# extends BASE
+# this represents a table
+class Restaurant(Base):
+    # representation of our table inside DB
+    __tablename__= 'restaurant'
+
+    # creating columns of table
+    name = Column(String(80), nullable = False)
+    # Primary Key
+    id = Column(Integer, primary_key = True)
+
+
+# extends BASE
+# this represents a table
+class MenuItem(Base):
+     __tablename__= 'menu_item'
+
+     name = Column(String(80), nullable = False)
+     # Primary Key
+     id = Column(Integer, primary_key = True)
+     course = Column(String(250))
+     description = Column(String(250))
+     price = Column(String(8))
+     # Foreign Key -> restaurant.id
+     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+     # creates relationship with class Restaurant
+     restaurant = relationship(Restaurant)
+
+# create instance of create database class
+# and point to db to use
+### insert at end of file ###
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
