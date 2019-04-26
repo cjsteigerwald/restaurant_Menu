10a7cd1264baa3b0805d4158bf7a4ab3e9cb616e
STEIGERWALD <cs741q@att.com>
2019-04-26 14:50:32 -0700

    Added routes and method definitions

--- finalProject.py
+++ finalProject.py
@@ -3,6 +3,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
 # Homepage that will display restaurants
 @app.route('/')
 @app.route('/restaurants/')
@@ -23,6 +33,27 @@ def editRestaurant():
 @app.route('/restaurant/restaurant_id/delete/')
 def deleteRestaurant():
     return "Page to delete restaurant"
+
+# Page for displaying restaurant menu
+@app.route('/restaurant/restaurant_id/')
+@app.route('/restaurant/restaurant_id/menu')
+def showMenu():
+    return "Show menu"
+
+# Page to add new menu item to restaurant
+@app.route('/restaurant/restaurant_id/menu/new/')
+def newMenuItem():
+    return "New menu item"
+
+# Page to edit menu item
+@app.route('/restaurant/restaurant_id/menu/edit/')
+def editMenuItem():
+    return "Edit menu item"
+
+# Page to delete menu item
+@app.route('/restaurant/restaurant_id/menu/delete/')
+def delteMenuItem():
+    return "Delete menu item"
 
 
 if __name__ == '__main__':
